a6fe566257f6db620756ae2a5402a52a2d9917c7
Meghan <darlinghirozerotwo002@gmail.com>
2024-03-31 10:00:57 +0800

    can't remember

---
+++ CategoryScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import CategoryScreen from './screens/CategoryScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
+import SettingScreen from './SettingScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Category" component={CategoryScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+        <Tab.Screen name="Contact" component={ContactScreen} />
+        <Tab.Screen name="Setting" component={SettingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
